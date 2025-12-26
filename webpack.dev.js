9bb41e5d942fc461da723cbff4627fa8bea8e3c6
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-12-26 14:41:20 +0800

    build, chore, refactor: add webpack common
    why:
    -  to resolve redundant code in webpack config files

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,38 +1,11 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
-module.exports = {
+module.exports = merge(common, {
   mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
   devtool: "eval-source-map",
   devServer: {
     watchFiles: ["./src/template.html"],
     static: "./dist",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-};
+});
